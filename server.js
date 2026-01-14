66c6d7840933d77b80774a953cfe5ec9f16e3506
MiltonJR7 <milton.oliveira.jr777@gmail.com>
2026-01-13 22:15:34 -0300

    Ajustando server

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set("trust proxy", 1);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 app.use(cookieParser());
 
 const sql = neon(process.env.DATABASE_URL);
